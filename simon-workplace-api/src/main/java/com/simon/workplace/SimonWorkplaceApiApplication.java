40d7447ef333af63197f6170bdf2261f9a44a51c
XiaoYang <2252712357@qq.com>
2026-06-12 17:08:29 +0800

    feat(api): scaffold Spring Boot service

---
+++ simon-workplace-api/src/main/java/com/simon/workplace/SimonWorkplaceApiApplication.java
@@ -0,0 +1,12 @@
+package com.simon.workplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimonWorkplaceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimonWorkplaceApiApplication.class, args);
+    }
+}
---
+++ simon-workplace-api/src/test/java/com/simon/workplace/SimonWorkplaceApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.simon.workplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimonWorkplaceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
